cda1881077181fb10d476920907ab362cc839928
qiu <Qiu-ux872>
2026-05-03 14:12:25 +0800

    作业

--- push.sh
+++ push.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+msg=$1
+
+# 1. 添加
 git add .
-git commit -m "$1"
+
+# 2. 提交
+git commit -m "$msg"
+
+# 3. 拉取并rebase（关键）
+git pull --rebase origin main
+
+# 4. 推送
 git push origin main
